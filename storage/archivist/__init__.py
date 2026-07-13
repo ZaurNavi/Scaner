3245ad650329cccbd437824def239edfd291deba
ZaurNavi <zaur.navi@gmail.com>
2026-07-13 17:01:41 +0400

    Update __init__.py

--- storage/archivist/__init__.py
+++ storage/archivist/__init__.py
@@ -1,4 +1,5 @@
 from .database import DatabaseManager
 from .migration import Migrator
+from .repository import Repository
 
-__all__ = ["DatabaseManager", "Migrator"]
+__all__ = ["DatabaseManager", "Migrator", "Repository"]
