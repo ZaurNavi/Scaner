6c8a5ce0e08056f5b1399266dd3931c4c725e214
ZaurNavi <zaur.navi@gmail.com>
2026-07-13 16:29:52 +0400

    Update device.py

--- storage/schema/device.py
+++ storage/schema/device.py
@@ -6,8 +6,8 @@ from .enums import DeviceStatus
 
 @dataclass(frozen=True)
 class Device:
+    mac: str  # <-- Сначала обязательные поля
     id: str = field(default_factory=lambda: str(uuid.uuid4()))
-    mac: str
     first_seen: datetime = field(default_factory=datetime.now)
     last_seen: datetime = field(default_factory=datetime.now)
     status: DeviceStatus = DeviceStatus.ACTIVE
