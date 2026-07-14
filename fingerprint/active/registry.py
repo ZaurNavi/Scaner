e86ad91b5110d87c646e86d32e8e32e9ec6b158e
ZaurNavi <zaur.navi@gmail.com>
2026-07-14 11:11:31 +0400

    Update registry.py

--- fingerprint/active/registry.py
+++ fingerprint/active/registry.py
@@ -13,9 +13,10 @@ from .snmp import SNMPCollector
 from .http import HTTPCollector
 from .switch_port import SwitchPortCollector
 from .netbios import NetBIOSCollector
-from .wsd import WSDCollector          # <-- ДОБАВЛЕНО
-from .ssh import SSHCollector          # <-- ДОБАВЛЕНО
-from .smb import SMBCollector          # <-- ДОБАВЛЕНО
+from .wsd import WSDCollector
+from .ssh import SSHCollector
+from .smb import SMBCollector
+from .https_cert import HTTPSCertCollector  # <-- ДОБАВЛЕНО
 
 
 # Единый реестр всех активных коллекторов
@@ -24,12 +25,13 @@ COLLECTORS: list[ActiveCollector] = [
     SNMPCollector(),
     SwitchPortCollector(),
     NetBIOSCollector(),
-    WSDCollector(),       # <-- ДОБАВЛЕНО
-    SSHCollector(),       # <-- ДОБАВЛЕНО
-    SMBCollector(),       # <-- ДОБАВЛЕНО
+    WSDCollector(),
+    SSHCollector(),
+    SMBCollector(),
     TCPCollector(),
     SSDPCollector(),
     HTTPCollector(),
+    HTTPSCertCollector(),  # <-- ДОБАВЛЕНО (после HTTP, т.к. использует TCP context)
 ]
 
 
