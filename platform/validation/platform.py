5e5c38669c50cd588f84034f783d4db9f7a08524
ZaurNavi <zaur.navi@gmail.com>
2026-07-16 23:36:46 +0400

    Create platform.py

---
+++ platform/validation/platform.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from typing import List
+
+class PlatformValidator:
+    @classmethod
+    def validate_all(cls) -> List[str]:
+        errors = []
+        # Здесь можно добавить вызовы validate_dependencies из Registry
+        return errors
