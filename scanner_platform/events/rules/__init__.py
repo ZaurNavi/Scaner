8d5d33d40688568371b2639608a34865cdc9f98a
ZaurNavi <zaur.navi@gmail.com>
2026-07-18 01:12:59 +0400

    Update __init__.py

--- scanner_platform/events/rules/__init__.py
+++ scanner_platform/events/rules/__init__.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
+"""Event Rules с автоматической регистрацией."""
 from .base_rule import BaseEventRule
 from .hostname_rule import HostnameRule
 from .vendor_rule import VendorRule
 from .capability_rule import CapabilityRule
 from .summary_rule import SummaryRule
+from .presence_rule import PresenceRule
+from .session_rule import SessionRule
+from ..registry import EventRuleRegistry
+
+# Автоматическая регистрация всех правил при импорте
+EventRuleRegistry.register(HostnameRule())
+EventRuleRegistry.register(VendorRule())
+EventRuleRegistry.register(CapabilityRule())
+EventRuleRegistry.register(SummaryRule())
+EventRuleRegistry.register(PresenceRule())
+EventRuleRegistry.register(SessionRule())
+
+__all__ = [
+    "BaseEventRule",
+    "HostnameRule",
+    "VendorRule",
+    "CapabilityRule",
+    "SummaryRule",
+    "PresenceRule",
+    "SessionRule",
+]
