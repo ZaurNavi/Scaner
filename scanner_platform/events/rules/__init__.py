2fb4d35ca55953e8a791eb27f0b0ee381a4cea5e
ZaurNavi <zaur.navi@gmail.com>
2026-07-18 01:01:59 +0400

    Create __init__.py

---
+++ scanner_platform/events/rules/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+from .base_rule import BaseEventRule
+from .hostname_rule import HostnameRule
+from .vendor_rule import VendorRule
+from .capability_rule import CapabilityRule
+from .summary_rule import SummaryRule
