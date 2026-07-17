b01c3dcecd9e09f5a15b7c2cc8d757b706c2eaad
ZaurNavi <zaur.navi@gmail.com>
2026-07-18 01:11:10 +0400

    Update capability_rule.py

--- scanner_platform/events/rules/capability_rule.py
+++ scanner_platform/events/rules/capability_rule.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Capability Change Rule."""
 from typing import Tuple
+from datetime import datetime
 from .base_rule import BaseEventRule
 from ..base import DomainEvent, EventOrigin
 from ..event_types import EventType
@@ -12,18 +13,19 @@ class CapabilityRule(BaseEventRule):
     def supports(self, change: Change) -> bool:
         return change.subject == "capability"
     
-    def emit(self, change: Change, diff_id: str) -> Tuple[DomainEvent, ...]:
+    def emit(self, change: Change, diff_id: str, device_uuid: str, occurred_at: datetime) -> Tuple[DomainEvent, ...]:
         events = []
         
         if change.type == ChangeType.ADDED:
             event = DomainEvent.create(
                 event_type=EventType.CAPABILITY_ADDED.value,
-                device_uuid=change.engine,
+                device_uuid=device_uuid,
                 payload={
                     "capability": change.new
                 },
                 source_diff_id=diff_id,
                 source_change_id=change.change_id,
+                occurred_at=occurred_at,
                 origin=EventOrigin.RULE
             )
             events.append(event)
@@ -31,12 +33,13 @@ class CapabilityRule(BaseEventRule):
         elif change.type == ChangeType.REMOVED:
             event = DomainEvent.create(
                 event_type=EventType.CAPABILITY_REMOVED.value,
-                device_uuid=change.engine,
+                device_uuid=device_uuid,
                 payload={
                     "capability": change.old
                 },
                 source_diff_id=diff_id,
                 source_change_id=change.change_id,
+                occurred_at=occurred_at,
                 origin=EventOrigin.RULE
             )
             events.append(event)
