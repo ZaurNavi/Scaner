42f60957b4f96f66eeacfeac6dd4ad679273edaa
ZaurNavi <zaur.navi@gmail.com>
2026-07-17 15:39:56 +0400

    Create __init__.py

---
+++ scanner_platform/profile/facets/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from .base import BaseFacet
+from .summary import SummaryFacet
+from .statistics import StatisticsFacet
+from .presence import PresenceFacet
+from .usage import UsageFacet
+from .behaviour import BehaviourFacet
+from .mobility import MobilityFacet
+from .capability import CapabilityFacet
