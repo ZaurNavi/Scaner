141443f8f85db7e4026c70086006a3a098392b9e
ZaurNavi <zaur.navi@gmail.com>
2026-07-18 00:59:34 +0400

    Create capability_rule.py

---
+++ scanner_platform/events/rules/capability_rule.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Capability Change Rule."""
+from typing import Tuple
+from .base_rule import BaseEventRule
+from ..base import DomainEvent, EventOrigin
+from ..event_types import EventType
+from ...diff.models import Change, ChangeType
+
+class CapabilityRule(BaseEventRule):
+    """Правило для изменений capabilities."""
+    
+    def supports(self, change: Change) -> bool:
+        return change.subject == "capability"
+    
+    def emit(self, change: Change, diff_id: str) -> Tuple[DomainEvent, ...]:
+        events = []
+        
+        if change.type == ChangeType.ADDED:
+            event = DomainEvent.create(
+                event_type=EventType.CAPABILITY_ADDED.value,
+                device_uuid=change.engine,
+                payload={
+                    "capability": change.new
+                },
+                source_diff_id=diff_id,
+                source_change_id=change.change_id,
+                origin=EventOrigin.RULE
+            )
+            events.append(event)
+        
+        elif change.type == ChangeType.REMOVED:
+            event = DomainEvent.create(
+                event_type=EventType.CAPABILITY_REMOVED.value,
+                device_uuid=change.engine,
+                payload={
+                    "capability": change.old
+                },
+                source_diff_id=diff_id,
+                source_change_id=change.change_id,
+                origin=EventOrigin.RULE
+            )
+            events.append(event)
+        
+        return tuple(events)
