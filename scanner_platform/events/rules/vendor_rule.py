7bf48a1f0ed892688bde0dad465bd3d39a7c55af
ZaurNavi <zaur.navi@gmail.com>
2026-07-18 01:10:50 +0400

    Update vendor_rule.py

--- scanner_platform/events/rules/vendor_rule.py
+++ scanner_platform/events/rules/vendor_rule.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Vendor Change Rule."""
 from typing import Tuple
+from datetime import datetime
 from .base_rule import BaseEventRule
 from ..base import DomainEvent, EventOrigin
 from ..event_types import EventType
@@ -16,7 +17,7 @@ class VendorRule(BaseEventRule):
             "vendor" in change.metadata.get("changed_fields", ())
         )
     
-    def emit(self, change: Change, diff_id: str) -> Tuple[DomainEvent, ...]:
+    def emit(self, change: Change, diff_id: str, device_uuid: str, occurred_at: datetime) -> Tuple[DomainEvent, ...]:
         old_vendor = change.old.get("vendor", "") if change.old else ""
         new_vendor = change.new.get("vendor", "") if change.new else ""
         
@@ -25,13 +26,14 @@ class VendorRule(BaseEventRule):
         # VendorChanged
         vendor_event = DomainEvent.create(
             event_type=EventType.VENDOR_CHANGED.value,
-            device_uuid=change.engine,
+            device_uuid=device_uuid,
             payload={
                 "old_vendor": old_vendor,
                 "new_vendor": new_vendor
             },
             source_diff_id=diff_id,
             source_change_id=change.change_id,
+            occurred_at=occurred_at,
             origin=EventOrigin.RULE
         )
         events.append(vendor_event)
@@ -39,7 +41,7 @@ class VendorRule(BaseEventRule):
         # DeviceIdentityChanged (дополнительное событие)
         identity_event = DomainEvent.create(
             event_type=EventType.DEVICE_IDENTITY_CHANGED.value,
-            device_uuid=change.engine,
+            device_uuid=device_uuid,
             payload={
                 "field": "vendor",
                 "old_value": old_vendor,
@@ -47,6 +49,7 @@ class VendorRule(BaseEventRule):
             },
             source_diff_id=diff_id,
             source_change_id=change.change_id,
+            occurred_at=occurred_at,
             origin=EventOrigin.RULE
         )
         events.append(identity_event)
